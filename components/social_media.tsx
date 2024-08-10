d3154c7c897ed16c0e58cf4e329dfcbc86443747
dayonaart <dayonaart@gmail.com>
2024-08-10 19:02:53 +0700

    update

--- components/social_media.tsx
+++ components/social_media.tsx
@@ -1,6 +1,6 @@
-import { openLink } from "@/lib/utils";
 import { useInView, Variants, motion } from "framer-motion";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useRef } from "react";
 import {
   FiDownload,
@@ -36,6 +36,7 @@ const MEDIA_SOCIAL = [
 ];
 
 const SocialMediaAndResume = ({ className }: { className?: string }) => {
+  const path = usePathname();
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
   const staggerVariant: Variants = {
@@ -67,7 +68,7 @@ const SocialMediaAndResume = ({ className }: { className?: string }) => {
         {MEDIA_SOCIAL.map((i) => (
           <motion.div key={i.link} variants={itemStaggerVariant}>
             <Link
-              href={i.link}
+              href={`${path == "/" ? `${i.link}` : `/${i.link}`}`}
               target="_blank"
               rel="noopener noreferrer"
               className="border p-2 rounded-full flex items-center hover:bg-cyan hover:text-white hover:transition-all duration-1000 hover:border-cyan/70"
